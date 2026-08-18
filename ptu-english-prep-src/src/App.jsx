4447bd5cd61c8f3b535cb28435ed6403d413cb2f
codingwithDev <165135910+dev913913@users.noreply.github.com>
2026-08-18 10:42:34 +0000

    Fix missing routes in App.jsx

--- ptu-english-prep-src/src/App.jsx
+++ ptu-english-prep-src/src/App.jsx
@@ -4,6 +4,9 @@ import Home from './pages/Home'
 import TrackPage from './pages/TrackPage'
 import UnitPage from './pages/UnitPage'
 import QuizPage from './pages/QuizPage'
+import ListeningHub from './pages/ListeningHub'
+import ListeningExercisePage from './pages/ListeningExercisePage'
+import LabPartPage from './pages/LabPartPage'
 
 export default function App() {
   return (
@@ -13,6 +16,9 @@ export default function App() {
         <Route path="/:track" element={<TrackPage />} />
         <Route path="/:track/:unitId" element={<UnitPage />} />
         <Route path="/:track/:unitId/quiz" element={<QuizPage />} />
+        <Route path="/:track/:unitId/listening" element={<ListeningHub />} />
+        <Route path="/:track/:unitId/listening/:exerciseId" element={<ListeningExercisePage />} />
+        <Route path="/:track/:unitId/:partId" element={<LabPartPage />} />
       </Routes>
     </Layout>
   )
